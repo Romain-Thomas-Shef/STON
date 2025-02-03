2875b5da9e769effb5d7fe20f7756132cc228273
Romain-Thomas-Shef <romain.thomas@sheffield.ac.uk>
2025-02-03 14:01:07 +0000

    test

--- ston/tests/test_processing_image_processing.py
+++ ston/tests/test_processing_image_processing.py
@@ -220,7 +220,7 @@ class META(unittest.TestCase):
                                              'test_meta_1image_with_text.png'))
 
 
-        print(final_image.size, test_image.size, numpy.array(final_image)-numpy.array(test_image))
+        print(final_image.size, test_image.size, numpy.where(numpy.array(final_image)-numpy.array(test_image) != 0))
         ##Check both image are the same
         self.assertTrue(numpy.array_equal(numpy.array(final_image),
                                           numpy.array(test_image)))
@@ -295,8 +295,8 @@ class META(unittest.TestCase):
         test_image = Image.open(os.path.join(data_directory,
                                              'test_meta_threeimage_2columns.png'))
 
-        print(final_image.size, test_image.size, numpy.array(final_image)-numpy.array(test_image))
 
+        print(final_image.size, test_image.size, numpy.where(numpy.array(final_image)-numpy.array(test_image) != 0))
         ##Check both image are the same
         self.assertTrue(numpy.array_equal(numpy.array(final_image),
                                           numpy.array(test_image)))
